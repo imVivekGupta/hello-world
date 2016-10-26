196e6530ce55da0ce63acea7d04426872aabee4f
imVivekGupta <vivekgupta3.vg@gmail.com>
2016-10-26 13:18:37 +0530

    Add files via upload

--- dfs.c
+++ dfs.c
@@ -1,35 +1,83 @@
 #include <stdio.h>
-#include <stdlib.h>
+int time;
 
-int dfs_visit(int G[8][8],int visited[8], int u, int flag){
-    int i;
-    visited[u] = 1;
-    printf("%d\n",u);
-    for(i=1;i<=7;i++){
-        if(G[u][i] && visited[i])
-          flag = 1;
-    }
-    for(i=1;i<=7;i++){
-        if(G[u][i] && !visited[i])
-           dfs_visit(G,visited,i,flag);
-    }
+void toposort(int finish[]){
+	int i,j,t;
+	int arr[6][2];
+	for(i=0;i<6;i++){
+		arr[i][0]=finish[i];
+		arr[i][1]=i;
+	}
+	for(i=0;i<6;i++)
+		for(j=0;j<5-i;j++)
+		{
+			if(arr[j][0]<arr[j+1][0]){
+				t=arr[j][0];
+				arr[j][0]=arr[j+1][0];
+				arr[j+1][0]=t;
+				t=arr[j][1];
+				arr[j][1]=arr[j+1][1];
+				arr[j+1][1]=t;
+			}
+		}
+		printf("\n");
+	for(i=0;i<6;i++)
+		printf("%d ",arr[i][1]);	
+	printf("\n");
 }
 
-void dfs(int G[8][8]){
-    int visited[8]={0,0,0,0,0,0,0,0};
-    int i,flag;
-    for(i=1;i<=7;i++){
-        if(visited[i]==0)
-            flag = dfs_visit(G,visited,i,0);
-    }
-    if(flag)
-    printf("Cyclic\n");
-    else
-    printf("Acyclic\n");
+void dfs_visit(int u,int G[6][6],char E[6][6],int visited[],int start[], int finish[]){
+	int v;
+	visited[u]=1;
+	printf("%d\n",u);
+	start[u]=++time;
+	for(v=0;v<6;v++){
+		if(G[u][v] && !visited[v]){
+			E[u][v] = 'T';
+			//E[u][v] = E[v][u] = 'T';
+			dfs_visit(v,G,E,visited,start,finish);
+		}	
+	}
+	finish[u]=++time;
+}
+
+void dfs(int G[6][6], char E[6][6]){
+	int visited[6]={0,0,0,0,0,0},i,j,time=0;
+	int start[6] = {0,0,0,0,0,0};
+	int finish[6] = {0,0,0,0,0,0};	
+	for(i=0;i<6;i++){
+		if(!visited[i])
+		dfs_visit(i,G,E,visited,start,finish);
+	}
+	printf("start :");
+	for(i=0;i<6;i++)
+		printf("%d ",start[i]);
+	printf("\nfinish:");
+	for(i=0;i<6;i++)
+		printf("%d ",finish[i]);
+	printf("\n");
+	for(i=0;i<6;i++)
+	{
+		for(j=0;j<6;j++){
+			if(G[i][j]){
+					if(start[j]<start[i] && start[i]<finish[i] && finish[i]<finish[j])
+						E[i][j] = 'B';
+					if(start[i]<start[j] && start[j]<finish[j] && finish[j]<finish[i] && E[i][j]!='T')
+						E[i][j] = 'F';
+					if(start[j]<finish[j] && finish[j]<start[i] && start[i]<finish[i])
+						E[i][j] = 'C';								
+					printf("%c ",E[i][j]);	
+			}
+			else
+				printf("* ");	
+			}
+		printf("\n");
+	}		
+	toposort(finish);	
 }
 
 int main(){
-    int G[8][8]={{0,0,0,0,0,0,0,0},{0,0,1,1,0,0,0,0},{0,0,0,0,0,1,1,0},{0,0,0,0,0,0,0,1},{0,0,1,0,0,0,0,0},{0,0,0,0,0,0,1,0},{0,0,0,0,0,0,0,1},{0,0,0,0,1,0,0,0}};
-    dfs(G);
-    return 0;
+	int G[6][6]={{0,1,0,0,0,1},{0,0,1,0,1,0},{0,0,0,1,0,0},{0,1,0,0,0,0},{0,0,1,0,0,1},{0,0,0,0,0,0}};
+	char E[6][6];
+	dfs(G,E);
 }
